8a2c17f2416cd6df3dac613bdd3aba2c39a5d967
Orhan <ya.eskimo-callboy@yandex.by>
2022-07-25 10:29:20 +0300

    no changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://qaq-api-dev.asist-lab.com',
+  apiUrl: 'https://qaq-api-dev.asist-lab.com:3000/api',
   // apiUrl: 'http://localhost:3500',
 };
