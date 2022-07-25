19561343f8976dbf90e5bdd47d93f9a3e8382d9f
Orhan <ya.eskimo-callboy@yandex.by>
2022-07-25 10:49:36 +0300

    fix: interceptor

--- src/app/helpers/user.interceptor.ts
+++ src/app/helpers/user.interceptor.ts
@@ -13,6 +13,7 @@ export class UserInterceptorService implements HttpInterceptor {
     let tokenizedReq = req.clone({
       setHeaders: {
         Authorization: `Bearer ${authService.getToken()}`,
+        'Access-Control-Allow-Origin': '**',
       },
     });
     return next.handle(tokenizedReq);
