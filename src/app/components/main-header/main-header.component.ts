37e31ff4608d4c665aa42dd62bb6523ea90d8a35
Orhan <ya.eskimo-callboy@yandex.by>
2022-08-24 11:57:50 +0300

    feat: menu button in main-header.component

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-main-header',
   templateUrl: './main-header.component.html',
@@ -9,7 +8,7 @@ import { Router } from '@angular/router';
 export class MainHeaderComponent implements OnInit {
   user!: any;
 
-  constructor(private _router: Router) {}
+  constructor(private _router: Router) { }
 
   ngOnInit(): void {
     let userString = localStorage.getItem('USER_TASTYCLUB');
